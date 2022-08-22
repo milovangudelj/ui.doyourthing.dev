02bf48bf203e9f090b7f36faa0f441e249b12ddd
milovangudelj <milovan.gudelj@gmail.com>
2022-08-22 23:03:29 +0200

    New `Link` component

--- src/components/atoms/Link/Link.tsx
+++ src/components/atoms/Link/Link.tsx
@@ -1,10 +1,29 @@
-export const Link = ({ href, label }: { href: string; label: string }) => {
-	return (
-		<a
-			href={href}
-			className="rounded bg-green-500 px-4 py-2 font-medium text-white"
-		>
-			{label}
+import { default as NextLink } from "next/link";
+
+export const Link = ({
+	href,
+	external = false,
+	children,
+	label,
+	className,
+}: {
+	href: string;
+	external?: boolean;
+	children?: React.ReactNode;
+	label?: string;
+	className?: string;
+}) => {
+	const classes =
+		`text-primary-500 underline hover:text-primary-400 active:text-primary-600` +
+		`${className ? " " + className : ""}`;
+
+	return !external ? (
+		<NextLink href={href} passHref>
+			<a className={classes}>{children ?? label}</a>
+		</NextLink>
+	) : (
+		<a href={href} className={classes}>
+			{children ?? label}
 		</a>
 	);
 };
