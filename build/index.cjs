a685cb8d075986c511c96a668de885b24d9c14c9
milovangudelj <milovan.gudelj@gmail.com>
2022-08-22 20:03:49 +0200

    Switched to rollup (oh god)

--- build/index.cjs
+++
@@ -1,7 +0,0 @@
-"use strict";
-
-if (process.env.NODE_ENV === "production") {
-	module.exports = require("./doyourthingui.prod.cjs");
-} else {
-	module.exports = require("./doyourthingui.dev.cjs");
-}
