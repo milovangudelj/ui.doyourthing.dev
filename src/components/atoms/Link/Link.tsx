9750573aca8334b441fc33e3da69606122d93c1f
milovangudelj <milovan.gudelj@gmail.com>
2022-08-22 23:22:48 +0200

    Switch back to regular link tag

--- src/components/atoms/Link/Link.tsx
+++ src/components/atoms/Link/Link.tsx
@@ -1,29 +1,19 @@
-import { default as NextLink } from "next/link";
-
 export const Link = ({
 	href,
-	external = false,
 	children,
-	label,
 	className,
 }: {
 	href: string;
-	external?: boolean;
 	children?: React.ReactNode;
-	label?: string;
 	className?: string;
 }) => {
 	const classes =
 		`text-primary-500 underline hover:text-primary-400 active:text-primary-600` +
 		`${className ? " " + className : ""}`;
 
-	return !external ? (
-		<NextLink href={href} passHref>
-			<a className={classes}>{children ?? label}</a>
-		</NextLink>
-	) : (
+	return (
 		<a href={href} className={classes}>
-			{children ?? label}
+			{children}
 		</a>
 	);
 };
