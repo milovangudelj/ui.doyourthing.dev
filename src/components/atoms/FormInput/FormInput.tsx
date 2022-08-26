a6e7e93b94a2aa006bf4045b6a053e423cf3f54a
milovangudelj <milovan.gudelj@gmail.com>
2022-08-26 11:49:53 +0200

    Back to primary focus color

--- src/components/atoms/FormInput/FormInput.tsx
+++ src/components/atoms/FormInput/FormInput.tsx
@@ -32,7 +32,7 @@ export const FormInput = ({ type, ...props }: FormInputProps) => {
 	};
 
 	return (
-		<div className="form-input flex w-full items-center rounded border-zinc-300 bg-transparent p-0 text-dark-he placeholder:text-dark-le focus-within:border-accent-500 focus-within:outline-none focus-within:ring-1 focus-within:ring-accent-500 dark:border-zinc-600 dark:text-light-he dark:placeholder:text-light-le focus-within:dark:border-accent-400 focus-within:dark:ring-accent-400">
+		<div className="form-input flex w-full items-center rounded border-zinc-300 bg-transparent p-0 text-dark-he placeholder:text-dark-le focus-within:border-primary-500 focus-within:outline-none focus-within:ring-1 focus-within:ring-primary-500 dark:border-zinc-600 dark:text-light-he dark:placeholder:text-light-le focus-within:dark:border-primary-400 focus-within:dark:ring-primary-400">
 			<Suspense fallback={<EmailInput />}>{element[type]}</Suspense>
 		</div>
 	);
